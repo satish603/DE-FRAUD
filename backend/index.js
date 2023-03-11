dc8dee68ee3c2ec661d6847c9f594e985707ef56
swarangimp05 <swarangimp05@gmail.com>
2023-03-11 22:52:17 +0530

    backend connect

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose=require('mongoose')
+var bodyParser = require('body-parser');
+const userModel = require("./models");
+const Verify = require("./verify_model");
+// 
+const DB=`mongodb+srv://defraudapp05:VeSeVvCmsbPxcHSE@defraud.cbywv1h.mongodb.net/?retryWrites=true&w=majority`
+
+
+mongoose.connect(DB,
+    
+    ).then(()=>{
+    console.log(`Connection successful`)
+}).catch((err)=> console.log(err));
+
+
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/add_product", async (request, response) => {
+  const user = new userModel(request.body);
+
+  try {
+    await user.save();
+    response.send(user);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/verify", async (request, response) => {
+    const users = await Verify.find({});
+
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+  });
+
+app.listen(8000, () => {
+  console.log("Listening on 8000")
+})
+
+module.exports = app;
